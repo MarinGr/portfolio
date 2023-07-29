6e8dd8f2676aac38a3cc11885734302f974c49ad
Marina Gr <marina.grishn@gmail.com>
2023-07-30 00:06:32 +0300

    add stack info

--- src/assets/data/stackData.jsx
+++ src/assets/data/stackData.jsx
@@ -3,16 +3,20 @@ import html from "../images/stackImages/html.png";
 import css from "../images/stackImages/css.png";
 import js from "../images/stackImages/js.png";
 import react from "../images/stackImages/react.png";
+import redux from "../images/stackImages/redux.png";
 import vuejs from "../images/stackImages/vuejs.png";
 import typescript from "../images/stackImages/typescript.png";
 import sass from "../images/stackImages/sass.png";
+import figma from "../images/stackImages/figma.png";
 
 export const stackData = [
   { id: uuidv4(), img: html, title: "HTML" },
   { id: uuidv4(), img: css, title: "CSS" },
   { id: uuidv4(), img: js, title: "Javascript" },
   { id: uuidv4(), img: react, title: "React" },
+  { id: uuidv4(), img: redux, title: "Redux" },
   { id: uuidv4(), img: vuejs, title: "Vue" },
   { id: uuidv4(), img: sass, title: "Sass" },
   { id: uuidv4(), img: typescript, title: "Typescript" },
+  { id: uuidv4(), img: figma, title: "Figma" },
 ];
